a6208d6bd9fd2c6965b365a735910f615b3693d8
Michael Place <mplace@solstice-consulting.com>
2013-11-28 13:41:05 -0600

    Initial Commit

---
+++ Dart/Dart/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Dart
+//
+//  Created by Michael Place on 11/28/13.
+//  Copyright (c) 2013 zheike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dart/Dart/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dart
+//
+//  Created by Michael Place on 11/28/13.
+//  Copyright (c) 2013 zheike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
